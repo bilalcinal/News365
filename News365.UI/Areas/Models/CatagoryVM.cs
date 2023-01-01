a0fbb990d77e4a14aa8d12b587a8b22fb7a25f5a
Bilal cinal <73084109+bilalcinal@users.noreply.github.com>
2023-01-01 14:50:57 +0300

    Areas cms Created

--- News365.UI/Areas/Models/CatagoryVM.cs
+++
@@ -1,8 +0,0 @@
-using News365.Entities.Concrete;
-
-namespace News365.UI.Areas.Models;
-
-    public class CatagoryVM
-    {
-        public Category Category { get; set; }
-    }
--- News365.UI/Models/ErrorViewModel.cs
+++ News365.UI/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace News365.UI.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+    public string RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
